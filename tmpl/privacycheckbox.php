b4139d6a2ee6344b8beb5f7b0c939cad239d4ac6
zero-24 <zero-24@users.noreply.github.com>
2020-03-24 02:09:31 +0100

    copy update; build scripts; joomla 4 compatibility

--- tmpl/privacycheckbox.php
+++ tmpl/privacycheckbox.php
@@ -2,7 +2,7 @@
 /**
  * PrivacyCheckbox Plugin
  *
- * @copyright  Copyright (C) 2018 Tobias Zulauf All rights reserved.
+ * @copyright  Copyright (C) 2018 - 2020 Tobias Zulauf All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
